88bbeb51fca7592328706f48e79cf5078860c631
thulana <thulanakannangara@gmail.com>
2017-08-21 10:36:51 +0530

    test changes

--- tests/acceptance/UATEnvironmentCest.php
+++ tests/acceptance/UATEnvironmentCest.php
@@ -6,25 +6,25 @@ class UATEnvironmentCest
     public function _before(AcceptanceTester $I)
     {
         $I->comment("Cloning project into /var/www/html");
-        $I->runShellCommand("docker exec uat_web git clone https://github.com/ChanakaR/php-simple.git /var/www/html/php-simple");
-        $I->runShellCommand('docker exec uat_web chmod 777 -R /var/www/html');
+        $I->runShellCommand("docker exec phantom_web git clone https://github.com/ChanakaR/php-simple.git /var/www/html/php-simple");
+        $I->runShellCommand('docker exec phantom_web chmod 777 -R /var/www/html');
     }
 
     public function _after(AcceptanceTester $I)
     {
         $I->comment("remove the project directory from /var/www/html");
-        $I->runShellCommand('docker exec uat_web rm -rf /var/www/html/php-simple');
+        $I->runShellCommand('docker exec phantom_web rm -rf /var/www/html/php-simple');
     }
 
     public function checkSampleApp(AcceptanceTester $I){
         $I->wantTo("verify uat environment is working properly with a php application");
-        $I->runShellCommand("docker exec uat_web php /var/www/html/php-simple/app.php");
+        $I->runShellCommand("docker exec phantom_web php /var/www/html/php-simple/app.php");
         $I->cantSeeInShellOutput("false");
     }
 
     public function checkLoginToDBFromPhpmyadmin(AcceptanceTester $I){
         $I->wantTo("log into mysql 5.5 server through phpmyadmin");
-        $I->amOnPage('http://localhost:9090');
+        $I->amOnPage('http://localhost:6768');
         $I->fillField('Username:', 'root');
         $I->fillField('Password:', '1234');
         $I->click('Go');
--- tests/unit/MysqlContainerCest.php
+++ tests/unit/MysqlContainerCest.php
@@ -14,20 +14,20 @@ class MysqlContainerCest
     // tests
     public function mySqlContainerTest(UnitTester $I){
         $I->wantTo("verify mysql container is up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} uat_mysql");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} phantom_mysql");
         $I->seeInShellOutput("true");
     }
 
     public function checkMySQLServiceIsRunning(AcceptanceTester $I){
         $I->wantTo("verify mysql 5.5 service is up and running");
         $I->runShellCommand("ping -c 30 localhost");
-        $I->runShellCommand("docker exec uat_mysql mysqladmin -uroot -p1234 status");
+        $I->runShellCommand("docker exec phantom_mysql mysqladmin -uroot -p1234 status");
         $I->seeInShellOutput("Uptime");
     }
 
     public function checkMySQLConfigurations(AcceptanceTester $I){
         $I->wantTo("verify my.cnf configuration is loaded");
-        $I->runShellCommand("docker exec uat_mysql mysql -uroot -p1234 -se 'show variables'");
+        $I->runShellCommand("docker exec phantom_mysql mysql -uroot -p1234 -se 'show variables'");
         $I->seeInShellOutput("event_scheduler	ON");
         $I->seeInShellOutput("innodb_log_buffer_size	8388608");
         $I->seeInShellOutput("innodb_buffer_pool_size	2147483648");
