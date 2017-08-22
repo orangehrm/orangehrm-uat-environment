1ac5a686d3a5765a46922c19f31d8b504c79d985
thulana <thulanakannangara@gmail.com>
2017-08-22 18:03:41 +0530

    changes in config

--- tests/acceptance/UATEnvironmentCest.php
+++ tests/acceptance/UATEnvironmentCest.php
@@ -6,25 +6,25 @@ class UATEnvironmentCest
     public function _before(AcceptanceTester $I)
     {
         $I->comment("Cloning project into /var/www/html");
-        $I->runShellCommand("docker exec uat_web git clone https://github.com/ChanakaR/php-simple.git /var/www/html/php-simple");
-        $I->runShellCommand('docker exec uat_web chmod 777 -R /var/www/html');
+        $I->runShellCommand("docker exec jade_web git clone https://github.com/ChanakaR/php-simple.git /var/www/html/php-simple");
+        $I->runShellCommand('docker exec jade_web chmod 777 -R /var/www/html');
     }
 
     public function _after(AcceptanceTester $I)
     {
         $I->comment("remove the project directory from /var/www/html");
-        $I->runShellCommand('docker exec uat_web rm -rf /var/www/html/php-simple');
+        $I->runShellCommand('docker exec jade_web rm -rf /var/www/html/php-simple');
     }
 
     public function checkSampleApp(AcceptanceTester $I){
         $I->wantTo("verify uat environment is working properly with a php application");
-        $I->runShellCommand("docker exec uat_web php /var/www/html/php-simple/app.php");
+        $I->runShellCommand("docker exec jade_web php /var/www/html/php-simple/app.php");
         $I->cantSeeInShellOutput("false");
     }
 
 //    public function checkSampleApp(AcceptanceTester $I){
 //        $I->wantTo("verify uat environment is working properly with a php application");
-//        $I->runShellCommand("docker exec uat_web php /var/www/html/OHRMStandalone/TEST/php-simple/app.php");
+//        $I->runShellCommand("docker exec jade_web php /var/www/html/OHRMStandalone/TEST/php-simple/app.php");
 //        $I->cantSeeInShellOutput("false");
 //    }
 
--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -13,81 +13,81 @@ class WebContainerCest
 
   public function checkContainerIsRunning(UnitTester $I){
         $I->wantTo("verify ubuntu container up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} uat_web");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} jade_web");
         $I->seeInShellOutput("true");
     }
 
 
     public function checkPHPVersion(UnitTester $I){
         $I->wantTo("verify php 7.0 is installed in the container");
-        $I->runShellCommand("docker exec uat_web php --version");
+        $I->runShellCommand("docker exec jade_web php --version");
         $I->seeInShellOutput('PHP 7.0');
     }
 
 //    public function checkSupervisorServiceIsRunning(UnitTester $I){
 //        $I->wantTo("verify apache is up and running in the container");
-//        $I->runShellCommand("docker exec uat_web service supervisor status");
+//        $I->runShellCommand("docker exec jade_web service supervisor status");
 //        $I->seeInShellOutput('supervisord is running');
 //    }
 
     public function checkApacheServiceIsRunning(UnitTester $I){
         $I->wantTo("verify apache is up and running in the container");
         $I->runShellCommand("ping -c 10 localhost");
-        $I->runShellCommand("docker exec uat_web service httpd status");
+        $I->runShellCommand("docker exec jade_web service httpd status");
         $I->seeInShellOutput('active (running)');
     }
 
     public function checkCronServiceIsRunning(UnitTester $I){
         $I->wantTo("verify cron is up and running in the container");
-        $I->runShellCommand("docker exec uat_web service crond status");
+        $I->runShellCommand("docker exec jade_web service crond status");
         $I->seeInShellOutput('active (running)');
     }
 
     public function checkMemcacheServiceIsRunning(UnitTester $I){
         $I->wantTo("verify apache is up and running in the container");
-        $I->runShellCommand("docker exec uat_web service memcached status");
+        $I->runShellCommand("docker exec jade_web service memcached status");
         $I->seeInShellOutput('active (running)');
     }
 
     public function checkSSHInstallation(UnitTester $I){
             $I->wantTo("verify OpenSSH is installed in the container");
-            $I->runShellCommand("docker exec uat_web rpm -qa | grep openssh-server");
+            $I->runShellCommand("docker exec jade_web rpm -qa | grep openssh-server");
             $I->seeInShellOutput("openssh-server-6.6.1p1-35");
     }
 
     public function checkSSHServiceRunning(UnitTester $I){
             $I->wantTo("verify ssh is up and running in the container");
-            $I->runShellCommand("docker exec uat_web service sshd status");
+            $I->runShellCommand("docker exec jade_web service sshd status");
             $I->seeInShellOutput('active (running)');
     }
 
     public function checkNSSPAMLDAPInstallation(UnitTester $I){
             $I->wantTo("verify nss-pam-ldapd is installed in the container");
-            $I->runShellCommand("docker exec uat_web rpm -qa | grep nss-pam-ldapd");
+            $I->runShellCommand("docker exec jade_web rpm -qa | grep nss-pam-ldapd");
             $I->seeInShellOutput("nss-pam-ldapd-0.8.13");
     }
 
     public function checkOpenldapInstallation(UnitTester $I){
             $I->wantTo("verify open-ldap is installed in the container");
-            $I->runShellCommand("docker exec uat_web rpm -qa | grep openldap");
+            $I->runShellCommand("docker exec jade_web rpm -qa | grep openldap");
             $I->seeInShellOutput("openldap-clients-2.4.40");
     }
 
     public function checkNSCDInstallation(UnitTester $I){
             $I->wantTo("verify nscd is installed in the container");
-            $I->runShellCommand("docker exec uat_web rpm -qa | grep nscd");
+            $I->runShellCommand("docker exec jade_web rpm -qa | grep nscd");
             $I->seeInShellOutput("nscd-2.17-157");
     }
 
     public function checkJavaVersion(UnitTester $I){
             $I->wantTo("verify java is installed in the container");
-            $I->runShellCommand("docker exec uat_web rpm -qa | grep java-1.8.0-openjdk");
+            $I->runShellCommand("docker exec jade_web rpm -qa | grep java-1.8.0-openjdk");
             $I->seeInShellOutput("java-1.8.0");
     }
 
     public function checkWgetVersion(UnitTester $I){
             $I->wantTo("verify wget is installed in the container");
-            $I->runShellCommand("docker exec uat_web rpm -qa | grep wget");
+            $I->runShellCommand("docker exec jade_web rpm -qa | grep wget");
             $I->seeInShellOutput("wget-1.14-13");
     }
     public function checkVHostConfig(UnitTester $I){
