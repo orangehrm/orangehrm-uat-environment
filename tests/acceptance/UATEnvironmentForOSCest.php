5dc3e2e52356bf2fb68ab4e16aacedb339de53c5
thulana <thulanakannangara@gmail.com>
2017-08-22 11:33:02 +0530

    add tests for orangehrm os

--- tests/acceptance/UATEnvironmentForOSCest.php
+++ tests/acceptance/UATEnvironmentForOSCest.php
@@ -8,6 +8,7 @@ class UATEnvironmentForOSCest
         $I->comment("Cloning project into /var/www/html");
         $I->runShellCommand("docker exec phantom_web git clone https://github.com/orangehrm/orangehrm.git /var/www/html/php-simple");
         $I->runShellCommand('docker exec phantom_web mkdir -p symfony/web');
+        $I->runShellCommand('docker exec phantom_web ls');
         $I->runShellCommand('docker exec phantom_web mv orangehrm symfony/web');
         $I->runShellCommand('docker exec phantom_web cd symfony/web/orangehrm && mv * ../');
         $I->runShellCommand('docker exec phantom_web chmod 777 -R /var/www/html');
