a9c92f103245bebc7298c3a9c9e44f82ef1532c1
thulana <thulanakannangara@gmail.com>
2017-08-22 10:24:24 +0530

    add tests for orangehrm os

---
+++ tests/acceptance/UATEnvironmentForOSCest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class UATEnvironmentForOSCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->comment("Cloning project into /var/www/html");
+        $I->runShellCommand("docker exec phantom_web wget -c http://downloads.sourceforge.net/project/orangehrm/stable/3.3.2/orangehrm-3.3.2.zip -O orangehrm-3.3.2.zip &&\
+                    unzip -o orangehrm-3.3.2.zip && \
+                    rm orangehrm-3.3.2.zip");
+        $I->runShellCommand('docker exec phantom_web chmod 777 -R /var/www/html');
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+        $I->comment("remove the project directory from /var/www/html");
+        $I->runShellCommand('docker exec phantom_web rm -rf /var/www/html/orangehrm-3.3.2');
+    }
+
+    public function checkOrangeHRMOSApp(AcceptanceTester $I){
+        $I->wantTo("verify uat environment is working properly with orangehrm opensource app");
+        $I->amOnPage('http://localhost:6767');
+        $I->cantSeeInShellOutput("anything");
+    }
+
+
+}
