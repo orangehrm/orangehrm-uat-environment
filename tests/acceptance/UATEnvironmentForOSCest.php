7680f50356af5b9fa88e1b2c3da8d2ad4728041b
thulana <thulanakannangara@gmail.com>
2017-08-23 16:47:35 +0530

    add acceptance test for os

--- tests/acceptance/UATEnvironmentForOSCest.php
+++ tests/acceptance/UATEnvironmentForOSCest.php
@@ -6,17 +6,16 @@ class UATEnvironmentForOSCest
     public function _before(AcceptanceTester $I)
     {
         $I->comment("Cloning project into /var/www/html");
-        $I->runShellCommand("docker exec phantom_web git clone https://github.com/orangehrm/orangehrm.git ");
-        $I->runShellCommand('docker exec phantom_web mkdir -p symfony/web');
-        $I->runShellCommand('docker exec phantom_web ls');
-        $I->runShellCommand('docker exec phantom_web cd symfony/web/orangehrm && mv * ../');
+
+        $I->runShellCommand('docker exec phantom_web bash -c "git clone https://github.com/orangehrm/orangehrm.git "');
+       // $I->runShellCommand('docker exec phantom_web bash -c "cd OHRMStandalone/TEST/symfony/web && mv * ../"');
         $I->runShellCommand('docker exec phantom_web chmod 777 -R /var/www/html');
     }
 
     public function _after(AcceptanceTester $I)
     {
         $I->comment("remove the project directory from /var/www/html");
-        $I->runShellCommand('docker exec phantom_web rm -rf /var/www/html/orangehrm');
+        $I->runShellCommand('docker exec phantom_web rm -rf orangehrm');
     }
 
     public function checkOrangeHRMOSApp(AcceptanceTester $I){
