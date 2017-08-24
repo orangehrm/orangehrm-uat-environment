80e8aa4a2a545c1cbce1e6d2aac6148737ffb81b
thulana <thulanakannangara@gmail.com>
2017-08-24 11:29:51 +0530

    add acceptance test for os

--- tests/acceptance/UATEnvironmentForOSCest.php
+++ tests/acceptance/UATEnvironmentForOSCest.php
@@ -11,7 +11,8 @@ class UATEnvironmentForOSCest
         $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm && bash fix_permissions.sh"');
         $I->runShellCommand('docker exec phantom_web bash -c "yes | cp -rf config.ini orangehrm/installer/config.ini"');
         $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm; composer install -d symfony/lib"');
-        //$I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm; php installer/cli_install.php 0"');
+        $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm/symfony; php symfony cc"');
+        $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm; php installer/cli_install.php 0"');
         $I->runShellCommand('docker exec phantom_web chmod 777 -R /var/www/html');
     }
 
