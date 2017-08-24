894dbbf9f74eebf3d844f457d3d5d500173a8e3d
thulana <thulanakannangara@gmail.com>
2017-08-24 11:20:09 +0530

    add acceptance test for os

--- tests/acceptance/UATEnvironmentForOSCest.php
+++ tests/acceptance/UATEnvironmentForOSCest.php
@@ -10,8 +10,8 @@ class UATEnvironmentForOSCest
         $I->runShellCommand('docker exec phantom_web bash -c "git clone https://github.com/orangehrm/orangehrm.git "');
         $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm && bash fix_permissions.sh"');
         $I->runShellCommand('docker exec phantom_web bash -c "yes | cp -rf config.ini orangehrm/installer/config.ini"');
-        //$I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm; composer install -d symfony/lib"');
-        $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm; php installer/cli_install.php 0"');
+        $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm; composer install -d symfony/lib"');
+        //$I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm; php installer/cli_install.php 0"');
         $I->runShellCommand('docker exec phantom_web chmod 777 -R /var/www/html');
     }
 
@@ -23,8 +23,8 @@ class UATEnvironmentForOSCest
 
     public function checkOrangeHRMOSApp(AcceptanceTester $I){
         $I->wantTo("verify uat environment is working properly with orangehrm opensource app");
-        $I->amOnPage('https://localhost:6767');
-        $I->see("OrangeHRM");
+        $I->amOnPage('https://localhost:6767/orangehrm');
+        $I->see("internal error");
     }
 
 
