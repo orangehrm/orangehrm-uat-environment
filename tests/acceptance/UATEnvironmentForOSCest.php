84d2993aa70d746ad8b94ccb8c5421e6417d784e
thulana <thulanakannangara@gmail.com>
2017-08-24 10:12:16 +0530

    add install step

--- tests/acceptance/UATEnvironmentForOSCest.php
+++ tests/acceptance/UATEnvironmentForOSCest.php
@@ -10,6 +10,7 @@ class UATEnvironmentForOSCest
         $I->runShellCommand('docker exec phantom_web bash -c "git clone https://github.com/orangehrm/orangehrm.git "');
         $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm && bash fix_permissions.sh"');
         $I->runShellCommand('docker exec phantom_web bash -c "yes | cp -rf config.ini orangehrm/installer/config.ini"');
+        $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm; composer install -d symfony/lib"');
         $I->runShellCommand('docker exec phantom_web bash -c "cd orangehrm; php installer/cli_install.php 0"');
         $I->runShellCommand('docker exec phantom_web chmod 777 -R /var/www/html');
     }
