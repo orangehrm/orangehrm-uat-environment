ebfc960a6193f0c59b5957a90e2c34dd37ed443f
thulana <thulanakannangara@gmail.com>
2017-08-28 17:14:58 +0530

    change test case

--- tests/acceptance/UATEnvironmentForOSCest.php
+++ tests/acceptance/UATEnvironmentForOSCest.php
@@ -22,120 +22,121 @@ class UATEnvironmentForOSCest
     }
 
 
-//
-//    public function testValidCredentials(AcceptanceTester $I)
-//    {
-//        $I->am('ohrm user');
-//        $I->wantTo('Login to application as admin');
-//        $I->lookForwardTo('access to orangehrm application');
-//        $I->amOnPage('https://localhost:6767/orangehrm');
-//        $I->fillField('txtUsername', 'Admin');
-//        $I->fillField('txtPassword', 'admin');
-//        $I->click('Submit');
-//        $I->see('Dashboard');
-//    }
-//    public function checkOrangeHRMOSApp(AcceptanceTester $I){
-//        $I->wantTo("verify uat environment is working properly with orangehrm opensource app");
-//        $I->amOnPage('https://localhost:6767/orangehrm');
-//        $I->see("internal error");
-//    }
-//
-//    /**
-//     *
-//     * @example{"empname":"testemp1","user":"testuser1","pword":"12345","role":"1"}
-//     * @example{"empname":"testemp2","user":"testuser2","pword":"12345","role":"2"}
-//     * @example{"empname":"testemp3","user":"testuser3","pword":"12345","role":"2"}
-//     *
-//     * */
-//
-//
-//    public function testAddUserInOHRMApp( AcceptanceTester $I, \Codeception\Example $example)
-//    {
-//        $I->am('ohrm user');
-//        $I->wantTo('check add user functionality');
-//        $I->lookForwardTo('access to orangehrm application and add 3 ess users');
-//        $I->amOnPage('https://localhost:6767/orangehrm');
-//        $I->fillField('txtUsername','admin');
-//        $I->fillField('txtPassword','admin');
-//        $I->click('Submit');
-//        $I->amOnPage('https://localhost:6767/orangehrm/symfony/web/index.php/pim/addEmployee');
-//        $I->fillField('firstName',$example['empname']);
-//        $I->fillField('lastName',$example['empname']);
-//        $I->checkOption('Create Login Details');
-//        $I->fillField('User Name',$example['user']);
-//        $I->fillField('Password',$example['pword']);
-//        $I->fillField('Confirm Password',$example['pword']);
-//
-//        $I->click('btnSave');
-//
-//        $I->see($example['empname']+' '+$example['empname']);
-//    }
-//
-//    /**
-//     *
-//     * @example{"empname":"testemp1","user":"testuser1","pword":"12345"}
-//     * @example{"empname":"testemp2","user":"testuser2","pword":"12345"}
-//     * @example{"empname":"testemp3","user":"testuser3","pword":"12345"}
-//     *
-//     * */
 
-//    public function testLoginWithNewUsersInOHRMApp(AcceptanceTester $I, \Codeception\Example $example)
-//    {
-//        $I->am('ohrm user');
-//        $I->wantTo('check login with new users');
-//        $I->lookForwardTo('access to orangehrm application using credentials of new users');
-//        $I->amOnPage('https://localhost:6767/orangehrm');
-//        $I->fillField('txtUsername', $example['user']);
-//        $I->fillField('txtPassword', $example['pword']);
-//        $I->click('Submit');
-//        $I->see('Dashboard');
-//    }
+    public function testValidCredentials(AcceptanceTester $I)
+    {
+        $I->am('ohrm user');
+        $I->wantTo('Login to application as admin');
+        $I->lookForwardTo('access to orangehrm application');
+        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm');
+        $I->fillField('txtUsername', 'Admin');
+        $I->fillField('txtPassword', 'admin');
+        $I->click('Submit');
+        $I->see('Dashboard');
+    }
+    public function checkOrangeHRMOSApp(AcceptanceTester $I){
+        $I->wantTo("verify uat environment is working properly with orangehrm opensource app");
+        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm');
+        $I->see("internal error");
+    }
+
+    /**
+     *
+     * @example{"empname":"testemp1","user":"testuser1","pword":"12345","role":"1"}
+     * @example{"empname":"testemp2","user":"testuser2","pword":"12345","role":"2"}
+     * @example{"empname":"testemp3","user":"testuser3","pword":"12345","role":"2"}
+     *
+     * */
+
+
+    public function testAddUserInOHRMApp( AcceptanceTester $I, \Codeception\Example $example)
+    {
+        $I->am('ohrm user');
+        $I->wantTo('check add user functionality');
+        $I->lookForwardTo('access to orangehrm application and add 3 ess users');
+        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm');
+        $I->fillField('txtUsername','admin');
+        $I->fillField('txtPassword','admin');
+        $I->click('Submit');
+        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm/symfony/web/index.php/pim/addEmployee');
+        $I->fillField('firstName',$example['empname']);
+        $I->fillField('lastName',$example['empname']);
+        $I->checkOption('Create Login Details');
+        $I->fillField('User Name',$example['user']);
+        $I->fillField('Password',$example['pword']);
+        $I->fillField('Confirm Password',$example['pword']);
+
+        $I->click('btnSave');
+
+        $I->see($example['empname']+' '+$example['empname']);
+    }
+
+    /**
+     *
+     * @example{"empname":"testemp1","user":"testuser1","pword":"12345"}
+     * @example{"empname":"testemp2","user":"testuser2","pword":"12345"}
+     * @example{"empname":"testemp3","user":"testuser3","pword":"12345"}
+     *
+     * */
+
+    public function testLoginWithNewUsersInOHRMApp(AcceptanceTester $I, \Codeception\Example $example)
+    {
+        $I->am('ohrm user');
+        $I->wantTo('check login with new users');
+        $I->lookForwardTo('access to orangehrm application using credentials of new users');
+        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm');
+        $I->fillField('txtUsername', $example['user']);
+        $I->fillField('txtPassword', $example['pword']);
+        $I->click('Submit');
+        $I->see('Dashboard');
+    }
 
 //    public function addLeavePeriod(AcceptanceTester $I)
 //    {
 //        $I->am('ohrm user');
 //        $I->wantTo('add leave period');
 //        $I->lookForwardTo('set leave period for whole year');
-//        $I->amOnPage('https://localhost:6767/orangehrm');
+//        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm');
 //        $I->fillField('txtUsername', 'Admin');
 //        $I->fillField('txtPassword', 'admin');
 //        $I->click('Submit');
 //        $I->see('Dashboard');
-//        $I->amOnPage('https://localhost:6767/orangehrm/symfony/web/index.php/leave/defineLeavePeriod');
+//        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm/symfony/web/index.php/leave/defineLeavePeriod');
 //        $I->selectOption('leaveperiod[cmbStartMonth]', '1');
 //        $I->selectOption('Start Date ', '1');
 //        $I->click('btnEdit');
 //
 //
 //    }
-//
-//    public function addLeaveType(AcceptanceTester $I)
-//    {
-//        $I->am('ohrm user');
-//        $I->wantTo('add leave types');
-//        $I->lookForwardTo('add new casual leave type');
-//        $I->amOnPage('https://localhost:6767/orangehrm');
-//        $I->fillField('txtUsername', 'Admin');
-//        $I->fillField('txtPassword', 'admin');
-//        $I->click('Submit');
-//        $I->see('Dashboard');
-//        $I->amOnPage('https://localhost:6767/orangehrm/symfony/web/index.php/leave/defineLeaveType');
-//        $I->fillField('leaveType[txtLeaveTypeName]', 'casual');
-//        $I->click('saveButton');
-//
-//
-//    }
+
+    public function addLeaveType(AcceptanceTester $I)
+    {
+        $I->am('ohrm user');
+        $I->wantTo('add leave types');
+        $I->lookForwardTo('add new casual leave type');
+        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm');
+        $I->fillField('txtUsername', 'Admin');
+        $I->fillField('txtPassword', 'admin');
+        $I->click('Submit');
+        $I->see('Dashboard');
+        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm/symfony/web/index.php/leave/defineLeaveType');
+        $I->fillField('leaveType[txtLeaveTypeName]', 'casual');
+        $I->click('saveButton');
+
+
+    }
+
 //    public function addEntailments(AcceptanceTester $I)
 //    {
 //        $I->am('ohrm user');
 //        $I->wantTo('add leave to a user');
 //        $I->lookForwardTo('add leave to a user');
-//        $I->amOnPage('https://localhost:6767/orangehrm');
+//        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm');
 //        $I->fillField('txtUsername', 'Admin');
 //        $I->fillField('txtPassword', 'admin');
 //        $I->click('Submit');
 //        $I->see('Dashboard');
-//        $I->amOnPage('https://localhost:6767/orangehrm/symfony/web/index.php/leave/addLeaveEntitlement');
+//        $I->amOnPage('https://localhost:6767/OpenSourceFreeHosting/orangehrm/symfony/web/index.php/leave/addLeaveEntitlement');
 //        $I->fillField('#entitlements_employee_empId', 'testemp1');
 //        $I->fillField('entitlements[entitlement]', 'annual');
 //        $I->click('btnSave');
