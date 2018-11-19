4bc5b65530c54edd6dea6acd4c07443dda56acf3
ChanakaR <bmcrathnayaka@gmail.com>
2018-11-19 17:41:55 +0530

    tests updated

--- tests/unit/MysqlContainerCest.php
+++ tests/unit/MysqlContainerCest.php
@@ -14,20 +14,20 @@ class MysqlContainerCest
     // tests
     public function mySqlContainerTest(UnitTester $I){
         $I->wantTo("verify mysql container is up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} infinity_mysql");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} jade_mysql");
         $I->seeInShellOutput("true");
     }
 
     public function checkMySQLServiceIsRunning(AcceptanceTester $I){
         $I->wantTo("verify mariadb 10.2 service is up and running");
         $I->runShellCommand("ping -c 30 localhost");
-        $I->runShellCommand("docker exec infinity_mysql mysqladmin -uroot -p1234 status");
+        $I->runShellCommand("docker exec jade_mysql mysqladmin -uroot -p1234 status");
         $I->seeInShellOutput("Uptime");
     }
 
     public function checkMySQLConfigurations(AcceptanceTester $I){
         $I->wantTo("verify my.cnf configuration is loaded");
-        $I->runShellCommand("docker exec infinity_mysql mysql -uroot -p1234 -se 'show variables'");
+        $I->runShellCommand("docker exec jade_mysql mysql -uroot -p1234 -se 'show variables'");
         $I->seeInShellOutput("event_scheduler	ON");
         $I->seeInShellOutput("innodb_log_buffer_size	8388608");
         $I->seeInShellOutput("innodb_buffer_pool_size	2147483648");
@@ -36,7 +36,7 @@ class MysqlContainerCest
 
     public function rabbitmqContainerTest(UnitTester $I){
         $I->wantTo("verify rabbitmq container is up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} infinity_rabbitmq");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} jade_rabbitmq");
         $I->seeInShellOutput("true");
     }
 
