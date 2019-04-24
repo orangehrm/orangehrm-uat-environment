1cc4cf46ba5095bba083bba7ac7ab5dcf1b95fce
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-04-24 17:59:18 +0530

    added thread sleep with 5 seconds

--- tests/unit/MysqlContainerCest.php
+++ tests/unit/MysqlContainerCest.php
@@ -19,18 +19,9 @@ class MysqlContainerCest
     }
 
     public function checkMySQLServiceIsRunning(AcceptanceTester $I){
-        $I->wantTo("verify mysql 5.5 service is up and running");
-        $I->runShellCommand("ping -c 30 localhost");
+        $I->wantTo("verify mysql 10.2 service is up and running");
+        sleep(5);
         $I->runShellCommand("docker exec phantom_mysql mysqladmin -uroot -p1234 status");
         $I->seeInShellOutput("Uptime");
-    }
-
-    public function checkMySQLConfigurations(AcceptanceTester $I){
-        $I->wantTo("verify my.cnf configuration is loaded");
-        $I->runShellCommand("docker exec phantom_mysql mysql -uroot -p1234 -se 'show variables'");
-        $I->seeInShellOutput("event_scheduler	ON");
-        $I->seeInShellOutput("innodb_log_buffer_size	8388608");
-        $I->seeInShellOutput("innodb_buffer_pool_size	2147483648");
-        $I->seeInShellOutput("max_allowed_packet	100663296");
     }
 }
