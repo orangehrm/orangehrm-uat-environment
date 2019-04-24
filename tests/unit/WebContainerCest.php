c8c806c9e1f9948fd27051cd6a49cf5b272571ef
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-04-24 18:26:54 +0530

    updated wait time for apache status test case

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -33,7 +33,7 @@ class WebContainerCest
 
     public function checkApacheServiceIsRunning(UnitTester $I){
         $I->wantTo("verify apache is up and running in the container");
-        sleep(3);
+        sleep(5);
         $I->runShellCommand("docker exec phantom_web service httpd status");
         $I->seeInShellOutput('active (running)');
     }
