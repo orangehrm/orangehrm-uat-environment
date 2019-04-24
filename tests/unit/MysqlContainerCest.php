11cd33f1f0f5447c18f615663db65b78fd77cd2f
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-04-24 17:17:58 +0530

    Updated MySQL Container test cases

--- tests/unit/MysqlContainerCest.php
+++ tests/unit/MysqlContainerCest.php
@@ -25,19 +25,4 @@ class MysqlContainerCest
         $I->seeInShellOutput("Uptime");
     }
 
-    public function checkMySQLConfigurations(AcceptanceTester $I){
-        $I->wantTo("verify my.cnf configuration is loaded");
-        $I->runShellCommand("docker exec infinity_mysql mysql -uroot -p1234 -se 'show variables'");
-        $I->seeInShellOutput("event_scheduler	ON");
-        $I->seeInShellOutput("innodb_log_buffer_size	8388608");
-        $I->seeInShellOutput("innodb_buffer_pool_size	2147483648");
-        $I->seeInShellOutput("max_allowed_packet	100663296");
-    }
-
-    public function rabbitmqContainerTest(UnitTester $I){
-        $I->wantTo("verify rabbitmq container is up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} infinity_rabbitmq");
-        $I->seeInShellOutput("true");
-    }
-
 }
