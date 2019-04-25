8d7c8b011bb354fe9b1cf414a71b2200ebbd3513
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-04-25 08:04:55 +0530

    test cases updated

--- tests/unit/MysqlContainerCest.php
+++ tests/unit/MysqlContainerCest.php
@@ -18,10 +18,10 @@ class MysqlContainerCest
         $I->seeInShellOutput("true");
     }
 
-    public function checkMySQLServiceIsRunning(AcceptanceTester $I){
-        $I->wantTo("verify mysql 5.5 service is up and running");
-        sleep(20);
-        $I->runShellCommand("docker exec phantom_mysql mysqladmin -uroot -p1234 status");
-        $I->seeInShellOutput("Uptime");
-    }
+//    public function checkMySQLServiceIsRunning(AcceptanceTester $I){
+//        $I->wantTo("verify mysql 5.5 service is up and running");
+//        sleep(20);
+//        $I->runShellCommand("docker exec phantom_mysql mysqladmin -uroot -p1234 status");
+//        $I->seeInShellOutput("Uptime");
+//    }
 }
