bfb7a3234a9f5f5ef83e0b8bf035ba440e8788f0
chanakar <bmcrathnayaka@gmail.com>
2020-04-20 23:05:00 +0530

    test cases updated

--- tests/acceptance/ContainerConnectionsCest.php
+++ tests/acceptance/ContainerConnectionsCest.php
@@ -5,22 +5,22 @@ class ContainerConnectionsCest
     public function _before(AcceptanceTester $I)
     {
         $I->comment("Cloning project into /var/www/html");
-        $I->runShellCommand("docker exec jade_web git clone -b db-creation https://github.com/orangehrm/environment-test-helpers.git db-creation");
-        $I->runShellCommand('docker exec jade_web chmod 777 -R /var/www/html');
+        $I->runShellCommand("docker exec guardian_web git clone -b db-creation https://github.com/orangehrm/environment-test-helpers.git db-creation");
+        $I->runShellCommand('docker exec guardian_web chmod 777 -R /var/www/html');
     }
 
     public function _after(AcceptanceTester $I)
     {
         $I->comment("remove the project directory from /var/www/html");
-        $I->runShellCommand('docker exec jade_web rm -rf /var/www/html/db-creation');
-        $I->runShellCommand('docker exec jade_web mysql -hdb -uroot -p1234 -e "drop database php_simple"');
+        $I->runShellCommand('docker exec guardian_web rm -rf /var/www/html/db-creation');
+        $I->runShellCommand('docker exec guardian_web mysql -hdb -uroot -p1234 -e "drop database php_simple"');
     }
 
     public function checkLoginToDBFromPhpmyadmin(AcceptanceTester $I){
         $I->wantTo("log into mysql 10.2 server through phpmyadmin");
-        $I->runShellCommand("docker exec jade_web php /var/www/html/db-creation/app.php");
+        $I->runShellCommand("docker exec guardian_web php /var/www/html/db-creation/app.php");
         $I->cantSeeInShellOutput("false");
-        $I->amOnPage('http://localhost:6869');
+        $I->amOnPage('http://localhost:6465');
         $I->fillField('Username:', 'root');
         $I->fillField('Password:', '1234');
         $I->click('Go');
--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -13,20 +13,20 @@ class WebContainerCest
 
   public function checkContainerIsRunning(UnitTester $I){
         $I->wantTo("verify ubuntu container up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} jade_web");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} guardian_web");
         $I->seeInShellOutput("true");
     }
 
 
     public function checkPHPVersion(UnitTester $I){
-        $I->wantTo("verify php 7.2 is installed in the container");
-        $I->runShellCommand("docker exec jade_web php --version");
-        $I->seeInShellOutput('PHP 7.2');
+        $I->wantTo("verify php 7.4 is installed in the container");
+        $I->runShellCommand("docker exec guardian_web php --version");
+        $I->seeInShellOutput('PHP 7.4');
     }
 
     public function checkForNologinFile(UnitTester $I){
         $I->wantTo("verify nologin file is not there");
-        $I->runShellCommand("docker exec jade_web ls /var/run/");
+        $I->runShellCommand("docker exec guardian_web ls /var/run/");
         $I->dontSeeInShellOutput("nologin");
     }
 
@@ -39,55 +39,55 @@ class WebContainerCest
 
     public function checkCronServiceIsRunning(UnitTester $I){
         $I->wantTo("verify cron is up and running in the container");
-        $I->runShellCommand("docker exec jade_web service crond status");
+        $I->runShellCommand("docker exec guardian_web service crond status");
         $I->seeInShellOutput('active (running)');
     }
 
     public function checkMemcacheServiceIsRunning(UnitTester $I){
         $I->wantTo("verify apache is up and running in the container");
-        $I->runShellCommand("docker exec jade_web service memcached status");
+        $I->runShellCommand("docker exec guardian_web service memcached status");
         $I->seeInShellOutput('active (running)');
     }
 
     public function checkSSHInstallation(UnitTester $I){
             $I->wantTo("verify OpenSSH is installed in the container");
-            $I->runShellCommand("docker exec jade_web rpm -qa | grep openssh-server");
+            $I->runShellCommand("docker exec guardian_web rpm -qa | grep openssh-server");
             $I->seeInShellOutput("openssh-server-7");
     }
 
     public function checkSSHServiceRunning(UnitTester $I){
             $I->wantTo("verify ssh is up and running in the container");
-            $I->runShellCommand("docker exec jade_web service sshd status");
+            $I->runShellCommand("docker exec guardian_web service sshd status");
             $I->seeInShellOutput('active (running)');
     }
 
     public function checkNSSPAMLDAPInstallation(UnitTester $I){
             $I->wantTo("verify nss-pam-ldapd is installed in the container");
-            $I->runShellCommand("docker exec jade_web rpm -qa | grep nss-pam-ldapd");
+            $I->runShellCommand("docker exec guardian_web rpm -qa | grep nss-pam-ldapd");
             $I->seeInShellOutput("nss-pam-ldapd-0.8");
     }
 
     public function checkOpenldapInstallation(UnitTester $I){
             $I->wantTo("verify open-ldap is installed in the container");
-            $I->runShellCommand("docker exec jade_web rpm -qa | grep openldap");
+            $I->runShellCommand("docker exec guardian_web rpm -qa | grep openldap");
             $I->seeInShellOutput("openldap-clients-2");
     }
 
     public function checkNSCDInstallation(UnitTester $I){
             $I->wantTo("verify nscd is installed in the container");
-            $I->runShellCommand("docker exec jade_web rpm -qa | grep nscd");
+            $I->runShellCommand("docker exec guardian_web rpm -qa | grep nscd");
             $I->seeInShellOutput("nscd-2");
     }
 
     public function checkJavaVersion(UnitTester $I){
             $I->wantTo("verify java is installed in the container");
-            $I->runShellCommand("docker exec jade_web rpm -qa | grep java-1.8.0-openjdk");
+            $I->runShellCommand("docker exec guardian_web rpm -qa | grep java-1.8.0-openjdk");
             $I->seeInShellOutput("java-1.8.0");
     }
 
     public function checkWgetVersion(UnitTester $I){
             $I->wantTo("verify wget is installed in the container");
-            $I->runShellCommand("docker exec jade_web rpm -qa | grep wget");
+            $I->runShellCommand("docker exec guardian_web rpm -qa | grep wget");
             $I->seeInShellOutput("wget-1");
     }
 //    public function checkVHostConfig(UnitTester $I){
@@ -102,19 +102,19 @@ class WebContainerCest
 
     public function checkRabbitMqStatus(UnitTester $I){
         $I->wantTo("verify docker is listening to rabbitMq queues");
-        $I->runShellCommand("netstat -ltpn | grep 6881");
-        $I->seeInShellOutput("6881");
+        $I->runShellCommand("netstat -ltpn | grep 6441");
+        $I->seeInShellOutput("6441");
     }
 
     public function checkRabbitMqManagementPlugin(UnitTester $I){
         $I->wantTo("verify docker is listening to rabbitMq management plugin");
-        $I->runShellCommand("netstat -ltpn | grep 6880");
-        $I->seeInShellOutput("6880");
+        $I->runShellCommand("netstat -ltpn | grep 6440");
+        $I->seeInShellOutput("6440");
     }
 
     public function checkSendMailNoArch(UnitTester $I){
         $I->wantTo("verify wether sendmail noarch is installed");
-        $I->runShellCommand("docker exec jade_web yum list installed | grep sendmail");
+        $I->runShellCommand("docker exec guardian_web yum list installed | grep sendmail");
         $I->seeInShellOutput("sendmail-cf.noarch");
     }
 
