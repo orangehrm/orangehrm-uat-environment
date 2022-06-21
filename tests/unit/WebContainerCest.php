d14f792ff54b886e6071b79a99d99e6de031bfa6
Pasindu Dissanayake <50499112+pasindu07@users.noreply.github.com>
2022-06-21 10:35:04 +0530

    container name updated

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -13,20 +13,20 @@ class WebContainerCest
 
   public function checkContainerIsRunning(UnitTester $I){
         $I->wantTo("verify ubuntu container up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} guardian_web");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} uat_web_rhel");
         $I->seeInShellOutput("true");
     }
 
 
     public function checkPHPVersion(UnitTester $I){
         $I->wantTo("verify php 7.4 is installed in the container");
-        $I->runShellCommand("docker exec guardian_web php --version");
+        $I->runShellCommand("docker exec uat_web_rhel php --version");
         $I->seeInShellOutput('PHP 7.4');
     }
 
     public function checkForNologinFile(UnitTester $I){
         $I->wantTo("verify nologin file is not there");
-        $I->runShellCommand("docker exec guardian_web ls /var/run/");
+        $I->runShellCommand("docker exec uat_web_rhel ls /var/run/");
         $I->dontSeeInShellOutput("nologin");
     }
 
@@ -39,73 +39,73 @@ class WebContainerCest
 
     public function checkCronServiceIsRunning(UnitTester $I){
         $I->wantTo("verify cron is up and running in the container");
-        $I->runShellCommand("docker exec guardian_web systemctl status crond");
+        $I->runShellCommand("docker exec uat_web_rhel systemctl status crond");
         $I->seeInShellOutput('active (running)');
     }
 
     // public function checkMemcacheServiceIsRunning(UnitTester $I){
     //     $I->wantTo("verify apache is up and running in the container");
-    //     $I->runShellCommand("docker exec guardian_web systemctl status memcached");
+    //     $I->runShellCommand("docker exec uat_web_rhel systemctl status memcached");
     //     $I->seeInShellOutput('active (running)');
     // }
 
     public function checkSSHInstallation(UnitTester $I){
             $I->wantTo("verify OpenSSH is installed in the container");
-            $I->runShellCommand("docker exec guardian_web rpm -qa | grep openssh-server");
+            $I->runShellCommand("docker exec uat_web_rhel rpm -qa | grep openssh-server");
             $I->seeInShellOutput("openssh-server-8");
     }
 
     public function checkSSHServiceRunning(UnitTester $I){
             $I->wantTo("verify ssh is up and running in the container");
-            $I->runShellCommand("docker exec guardian_web systemctl status sshd");
+            $I->runShellCommand("docker exec uat_web_rhel systemctl status sshd");
             $I->seeInShellOutput('active (running)');
     }
 
     public function checkSSSDInstallation(UnitTester $I){
         $I->wantTo("verify sssd is installed in the container");
-        $I->runShellCommand("docker exec guardian_web rpm -qa | grep sssd");
+        $I->runShellCommand("docker exec uat_web_rhel rpm -qa | grep sssd");
         $I->seeInShellOutput('sssd-2');
     }
 
     public function checkSSSDServiceRunning(UnitTester $I){
         $I->wantTo("verify sssd is up and running in the container");
-        $I->runShellCommand("docker exec guardian_web systemctl status sssd");
+        $I->runShellCommand("docker exec uat_web_rhel systemctl status sssd");
         $I->seeInShellOutput('active (running)');
     }
 
     public function checkOddJobMkHomeDirInstallation(UnitTester $I){
             $I->wantTo("verify oddjob-mkhomedir is installed in the container");
-            $I->runShellCommand("docker exec guardian_web rpm -qa | grep oddjob-mkhomedir");
+            $I->runShellCommand("docker exec uat_web_rhel rpm -qa | grep oddjob-mkhomedir");
             $I->seeInShellOutput("oddjob-mkhomedir-0");
     }
 
     public function checkOpenldapInstallation(UnitTester $I){
             $I->wantTo("verify open-ldap is installed in the container");
-            $I->runShellCommand("docker exec guardian_web rpm -qa | grep openldap");
+            $I->runShellCommand("docker exec uat_web_rhel rpm -qa | grep openldap");
             $I->seeInShellOutput("openldap-clients-2");
     }
 
     public function checkOpensslPerlInstallation(UnitTester $I){
         $I->wantTo("verify openssl-perl is installed in the container");
-        $I->runShellCommand("docker exec guardian_web rpm -qa | grep openssl-perl");
+        $I->runShellCommand("docker exec uat_web_rhel rpm -qa | grep openssl-perl");
         $I->seeInShellOutput("openssl-perl-1");
     }
 
     // public function checkNSCDInstallation(UnitTester $I){
     //         $I->wantTo("verify nscd is installed in the container");
-    //         $I->runShellCommand("docker exec guardian_web rpm -qa | grep nscd");
+    //         $I->runShellCommand("docker exec uat_web_rhel rpm -qa | grep nscd");
     //         $I->seeInShellOutput("nscd-2");
     // }
 
     public function checkJavaVersion(UnitTester $I){
             $I->wantTo("verify java is installed in the container");
-            $I->runShellCommand("docker exec guardian_web rpm -qa | grep java-1.8.0-openjdk");
+            $I->runShellCommand("docker exec uat_web_rhel rpm -qa | grep java-1.8.0-openjdk");
             $I->seeInShellOutput("java-1.8.0");
     }
 
     public function checkWgetVersion(UnitTester $I){
             $I->wantTo("verify wget is installed in the container");
-            $I->runShellCommand("docker exec guardian_web rpm -qa | grep wget");
+            $I->runShellCommand("docker exec uat_web_rhel rpm -qa | grep wget");
             $I->seeInShellOutput("wget-1");
     }
 //    public function checkVHostConfig(UnitTester $I){
@@ -121,7 +121,7 @@ class WebContainerCest
 
     public function checkSendMailNoArch(UnitTester $I){
         $I->wantTo("verify whether sendmail noarch is installed");
-        $I->runShellCommand("docker exec guardian_web dnf list installed | grep sendmail");
+        $I->runShellCommand("docker exec uat_web_rhel dnf list installed | grep sendmail");
         $I->seeInShellOutput("sendmail-cf.noarch");
     }
 
