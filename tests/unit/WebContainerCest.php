8738454e96c1466cc9197451d77e405875d70ae3
deeghayu <deeghayu@orangehrm.com>
2025-01-15 12:14:37 +0530

    Updated the sssd test cases

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -55,11 +55,11 @@ class WebContainerCest
             $I->seeInShellOutput("openssh-server-8");
     }
 
-    //public function checkSSHServiceRunning(UnitTester $I){
-    //        $I->wantTo("verify ssh is up and running in the container");
-    //        $I->runShellCommand("docker exec uat_web_rhel systemctl status sshd");
-    //        $I->seeInShellOutput('active (running)');
-    //}
+    public function checkSSHServiceRunning(UnitTester $I){
+            $I->wantTo("verify ssh is up and running in the container");
+            $I->runShellCommand("docker exec uat_web_rhel systemctl status sshd");
+            $I->seeInShellOutput('active (running)');
+    }
 
     public function checkSSSDInstallation(UnitTester $I){
         $I->wantTo("verify sssd is installed in the container");
@@ -67,11 +67,11 @@ class WebContainerCest
         $I->seeInShellOutput('sssd-2');
     }
 
-    public function checkSSSDServiceRunning(UnitTester $I){
-        $I->wantTo("verify sssd is up and running in the container");
-        $I->runShellCommand("docker exec uat_web_rhel systemctl status sssd");
-        $I->seeInShellOutput('active (running)');
-    }
+    //public function checkSSSDServiceRunning(UnitTester $I){
+    //    $I->wantTo("verify sssd is up and running in the container");
+    //    $I->runShellCommand("docker exec uat_web_rhel systemctl status sssd");
+    //    $I->seeInShellOutput('active (running)');
+    //}
 
     public function checkOddJobMkHomeDirInstallation(UnitTester $I){
             $I->wantTo("verify oddjob-mkhomedir is installed in the container");
